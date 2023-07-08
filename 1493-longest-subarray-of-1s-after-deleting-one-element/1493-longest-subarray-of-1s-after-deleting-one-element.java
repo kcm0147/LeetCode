0ea9d6f821ad0088b712a40a700b9d2a8f8ef622
Chang Muk Kim <kcm0147@hanmail.net>
2023-07-08 15:47:30 +0900

    Time: 2 ms (84.24%), Space: 55.7 MB (5.28%) - LeetHub

--- 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.java
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.java
@@ -4,34 +4,22 @@ class Solution {
         int cur = 0;
         int prev = 0;
         int maxResult = 0;
-        boolean existZero = false;
+
         
         for(int i=0;i<nums.length;i++){
             if(nums[i]==1){
                 cur++;
             }
-            
-            if(nums[i]== 0){
+            if(nums[i]==0 || i==nums.length-1){
                 prev+=cur;
                 maxResult = Math.max(maxResult,prev);
                 prev = cur;
                 maxResult = Math.max(maxResult,cur);
                 cur=0;
-                existZero = true;
-            }
-            else if(i == nums.length-1){
-                prev+=cur;
-                maxResult = Math.max(maxResult,prev);
-                prev = cur;
-                maxResult = Math.max(maxResult,cur);
-                cur=0;
-            }
-            
+            }   
         }
-        if(!existZero){
-            maxResult-=1;
-        }
+       
         
-        return maxResult;
+        return maxResult == nums.length ? maxResult-1 : maxResult;
     }
 }
