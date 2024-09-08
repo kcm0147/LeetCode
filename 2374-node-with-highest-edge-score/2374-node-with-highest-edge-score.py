a2c532af3921ffefa7bc080d64f9a5a38df0a208
Chang Muk Kim <kcm0147@hanmail.net>
2024-09-08 22:41:38 +0900

    Time: 880 ms (74.59%), Space: 30.8 MB (84.43%) - LeetHub

--- 2374-node-with-highest-edge-score/2374-node-with-highest-edge-score.py
+++ 2374-node-with-highest-edge-score/2374-node-with-highest-edge-score.py
@@ -1,15 +1,8 @@
 class Solution:
     def edgeScore(self, edges: List[int]) -> int:
-        graph = {}
         sum_ary = [0] * len(edges)
-        max_value = -1
-        max_index = -1
 
         for index, target in enumerate(edges):
-            graph.setdefault(target, []).append(index)
             sum_ary[target] += index
-        for index, current_sum in enumerate(sum_ary):
-            if max_value < current_sum:
-                max_value = current_sum
-                max_index = index
-        return max_index
+
+        return max(range(len(sum_ary)), key=lambda i: sum_ary[i])
